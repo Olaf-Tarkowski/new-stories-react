8de98af7f3404e24f593761d745817eb18736a3b
Olaf-Tarkowski <tarkowski.software@gmail.com>
2022-11-24 00:11:12 +0100

    corrected App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./common/Footer";
+import Header from "./common/Header";
+import HomePage from "./common/HomePage";
+import { GlobalContainer } from "./styled";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalContainer>
+      <Header />
+      <HomePage />
+      <Footer />
+    </GlobalContainer>
   );
 }
 
