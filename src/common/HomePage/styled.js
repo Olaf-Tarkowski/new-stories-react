1de34830f4f6e6a5a7fcb935210120fa747a6ab0
Olaf-Tarkowski <tarkowski.software@gmail.com>
2022-11-24 00:17:22 +0100

    Built styling for HompPage

---
+++ src/common/HomePage/styled.js
@@ -0,0 +1,123 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin: 214px 0 0 0;
+`;
+
+export const GreetingContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const LogoPng = styled.img`
+  width: 266px;
+  height: 185px;
+`;
+
+export const ShortText = styled.div`
+  margin: 90px 0 43px 0;
+  font-family: "neonoir", sans-serif;
+  font-size: 30px;
+  text-align: center;
+  color: #a59f90;
+  width: 400px;
+  height: 74px;
+`;
+
+export const Text = styled.div`
+  margin-bottom: 69px;
+  font-family: "adobe-garamond-pro", serif;
+  font-size: 30px;
+  line-height: 42px;
+  letter-spacing: 6px;
+  text-align: center;
+  width: 1060px;
+  height: 72px;
+`;
+
+export const CreamButton = styled.button`
+  background: #fbd5ca;
+  border: none;
+  font-size: 16px;
+  font-weight: 600;
+  letter-spacing: 0.96px;
+  width: 230px;
+  height: 70px;
+  transition: 0.9s;
+  :hover {
+    transform: scale(1.09);
+    cursor: pointer;
+  }
+  :active {
+    background: #cae5fb;
+  }
+`;
+
+export const BuildingJpg = styled.img`
+  margin: 70px 0 0 0;
+  width: 1920px;
+  height: 810px;
+`;
+
+export const SmallTextContainer = styled.div`
+  display: flex;
+`;
+
+export const LeftText = styled.div`
+  width: 381px;
+  margin: 101px 210px 0 377px;
+`;
+
+export const RightText = styled.div`
+  margin-top: 160px;
+  width: 536px;
+`;
+
+export const NeoFontWord = styled.div`
+  margin-bottom: 30px;
+  font-family: "neonoir", sans-serif;
+  font-size: 26px;
+  color: #a59f90;
+`;
+
+export const AdoFontWord = styled.div`
+  margin-bottom: 81px;
+  width: 400px;
+  font-family: "adobe-garamond-pro", serif;
+  font-size: 30px;
+  line-height: 42px;
+  letter-spacing: 6px;
+`;
+
+export const AdoItaFontWord = styled.div`
+  font-family: "adobe-garamond-pro", serif;
+  font-style: italic;
+  font-size: 24px;
+  line-height: 34px;
+  letter-spacing: 0.72px;
+`;
+
+export const ReaFontWord = styled.div`
+  margin-top: 80px;
+  font-size: 16px;
+  line-height: 32px;
+`;
+
+export const Line = styled.div`
+  border-top: 1px solid #0b1036;
+  width: 131px;
+`;
+
+export const TeaserText = styled.div`
+  margin: 60px 0 60px;
+  width: 280px;
+  font-size: 18px;
+  letter-spacing: 3.6px;
+`;
+
+export const CreamText = styled.div`
+  margin-top: 18px;
+  color: #A59F90;
+`;
