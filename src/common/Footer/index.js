867d4b995baa67d4c9aeea32eaf00098c39b6268
Olaf-Tarkowski <tarkowski.software@gmail.com>
2022-12-06 20:09:25 +0100

    Split Footer for one more Component

--- src/common/Footer/index.js
+++ src/common/Footer/index.js
@@ -1,15 +1,8 @@
-import bacgroundPng from "../../resources/images/palac_bialy.png";
-import buildingJpg from "../../resources/images/KADR_02_8.jpg";
-import handPng from "../../resources/images/ręka.png";
 import logoNewStories from "../../resources/images/nowe_historie.png";
 import logoHvilePark from "../../resources/images/HvilePark.svg";
 import logoPalaceBraci from "../../resources/images/Palac_logo.svg";
 import {
-  DottedBuildingPng,
-  BuildingJpg,
-  HideOverflow,
   Container,
-  HandPng,
   ThreeColContainer,
   LogoNewStories,
   LogoHvilePark,
@@ -17,66 +10,11 @@ import {
   BasicInfo,
   Line,
   InvitationText,
-  NeoFontWord,
-  AdoFontWord,
-  AdoItaFontWord,
-  ReaFontWord,
-  Border,
-  InfoBox,
-  SmallBox,
-  Box,
-  BigBox,
-  AdoBiggerSizeFontWord,
-  Input,
-  InputContainer,
-  InputCheckBox,
-  LabelCheckBox,
-  CheckBoxContainer,
-  CreamButton,
 } from "./styled";
 
 const Footer = () => {
   return (
     <Container>
-      <HideOverflow>
-        <DottedBuildingPng src={bacgroundPng} />
-        <BigBox>
-          <SmallBox>
-            <NeoFontWord>Spotkanie z nami</NeoFontWord>
-            <AdoFontWord>UMÓWMY PREZENTACJĘ</AdoFontWord>
-            <InfoBox>
-              <Border>
-                <AdoItaFontWord>+48 502 567 144</AdoItaFontWord>
-                <ReaFontWord>Zadzwoń</ReaFontWord>
-              </Border>
-              <Border>
-                <AdoItaFontWord>biuro@nowehistorie.pl</AdoItaFontWord>
-                <ReaFontWord>Napisz</ReaFontWord>
-              </Border>
-            </InfoBox>
-          </SmallBox>
-          <Box>
-            <NeoFontWord>Zostaw swoje dane</NeoFontWord>
-            <AdoBiggerSizeFontWord>ODDZWONIMY</AdoBiggerSizeFontWord>
-            <InputContainer>
-              <Input type="text" placeholder="Imię*" />
-              <Input type="text" placeholder="Nazwisko*" />
-              <Input type="text" placeholder="Adres e-mail*" />
-              <Input type="text" placeholder="Numer telefonu*" />
-            </InputContainer>
-            <CheckBoxContainer>
-              <InputCheckBox type="checkbox" />
-              <LabelCheckBox>
-                Wyrażam zgodę na lorem ipsum dolor sit amet, consetetur
-                sadipscing …
-              </LabelCheckBox>
-            </CheckBoxContainer>
-            <CreamButton>Wyślij wiadomość</CreamButton>
-          </Box>
-        </BigBox>
-      </HideOverflow>
-      <BuildingJpg src={buildingJpg} />
-      <HandPng src={handPng} />
       <InvitationText>ZAPRASZAMY DO NASZEGO BIURA SPRZEDAŻY.</InvitationText>
       <ThreeColContainer>
         <BasicInfo>
--- src/common/Footer/styled.js
+++ src/common/Footer/styled.js
@@ -1,194 +1,15 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  margin: 100px 0 85px;
-  color: #fff;
-  position: relative;
-`;
-
-export const HideOverflow = styled.div`
-  overflow: hidden;
-  position: relative;
-  width: 1920px;
-  height: 900px;
-  background-color: #0b1036;
-`;
-
-export const DottedBuildingPng = styled.img`
-  position: absolute;
-  top: 249px;
-  right: -630px;
-  width: 1024px;
-  height: 674px;
-`;
-
-export const BuildingJpg = styled.img`
-  position: absolute;
-  top: -22px;
-  left: 228px;
-  width: 576px;
-  height: 461px;
-`;
-
-export const HandPng = styled.img`
-  position: absolute;
-  top: 378px;
-  left: 230px;
-  width: 134px;
-  height: 185px;
-
-  @keyframes up-and-down {
-    0% {
-      transform: translateY(-20px);
-    }
-    100% {
-      transform: translateY(20px);
-    }
-  }
-  animation: up-and-down 3s infinite;
-  animation-direction: alternate;
-`;
-
-export const BigBox = styled.div`
-  display: flex;
-`;
-
-export const SmallBox = styled.div`
-  margin: 588px 0 0 228px;
-`;
-
-export const InfoBox = styled.div`
-  width: 554px;
-  display: flex;
-  justify-content: space-between;
-`;
-
-export const Border = styled.div`
-  width: 260px;
-  border: 1px solid #a59f90;
-  border-width: 1px 0 1px 0;
-`;
-
-export const Box = styled.div`
-  margin: 218px 0 0 184px;
-`;
-
-export const InputContainer = styled.div`
-  width: 708px;
-  display: grid;
-  grid-template-columns: 50% 50%;
-  column-gap: 16px;
-  row-gap: 16px;
-`;
-
-export const Input = styled.input`
-  max-width: 724px;
-  padding: 26px 19px;
-  font-family: "Readex Pro", sans-serif;
-  font-size: 14px;
-  letter-spacing: 0.84px;
-  border: 1px solid #a59f90;
-  color: #fff;
-  background-color: transparent;
-  ::placeholder {
-    color: #fff;
-  }
-  :nth-child(1) {
-    grid-column: 1/2;
-  }
-  :nth-child(3) {
-    grid-column: 1/4;
-  }
-  :nth-child(4) {
-    grid-column: 1/4;
-  }
-`;
-
-export const CheckBoxContainer = styled.label`
-  margin: 26px 0;
-  display: flex;
-  align-items: center;
-`;
-
-export const InputCheckBox = styled.div`
-  margin: 0 26px 0 0;
-  width: 30px;
-  height: 30px;
-  border: 1px solid #a59f90;
-  :hover {
-    cursor: pointer;
-  }
-`;
-
-export const LabelCheckBox = styled.label`
-  font-size: 12px;
-`;
-
-
-export const CreamButton = styled.button`
-  position: absolute;
-  background-color: #fbd5ca;
+  margin-bottom: 85px;
+  font-family: "adobe-garamond-pro", serif;
   text-align: center;
-  font-family: "Readex Pro", sans-serif;
-  font-size: 14px;
-  font-weight: bold;
-  letter-spacing: 0.84px;
-  width: 724px;
-  height: 70px;
-  border: none;
-  transition: 0.9s;
-  :hover {
-    transform: scale(1.04);
-    cursor: pointer;
-  }
-  :active {
-    background: #cae5fb;
-  }
-`;
-
-export const NeoFontWord = styled.div`
-  font-family: "neonoir", sans-serif;
-  font-size: 26px;
-  color: #fbd5ca;
-`;
-
-export const AdoFontWord = styled.div`
-  margin: 21px 0 37px;
-  width: 460px;
-  font-family: "adobe-garamond-pro", serif;
-  font-size: 30px;
-  height: 30px;
-  letter-spacing: 6px;
-`;
-
-export const AdoBiggerSizeFontWord = styled.div`
-  margin: 19px 0 54px;
-  font-family: "adobe-garamond-pro", serif;
-  font-size: 60px;
-  height: 61px;
-  letter-spacing: 12px;
-`;
-
-export const AdoItaFontWord = styled.div`
-  margin: 12px 0 2px;
-  font-family: "adobe-garamond-pro", serif;
-  font-style: italic;
-  font-size: 24px;
-  line-height: 32px;
-  letter-spacing: 0.72px;
-`;
-
-export const ReaFontWord = styled.div`
-  margin-bottom: 12px;
-  font-size: 14px;
-  letter-spacing: 0.84px;
+  color: #0b1036;
+  position: relative;
 `;
 
 export const InvitationText = styled.div`
   margin: 58px 0 69px;
-  color: #0b1036;
-  text-align: center;
-  font-family: "adobe-garamond-pro", serif;
   font-size: 30px;
   line-height: 42px;
   letter-spacing: 6px;
@@ -203,9 +24,6 @@ export const ThreeColContainer = styled.div`
 `;
 
 export const BasicInfo = styled.div`
-  color: #0b1036;
-  text-align: center;
-  font-family: "adobe-garamond-pro", serif;
   font-size: 24px;
   font-style: italic;
   line-height: 34px;
